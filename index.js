1301752c28925e2cc685319837d2bec2f5ca5683
hidekazu <hidekazu@192.168.10.106>
2018-03-07 22:36:09 +0900

    紹介ページ更新

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ $(document).ready(function(){
 //	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
 //	});
     $('#fullpage').fullpage({
-    	sectionsColor: ['#000', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-    	anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
-    	menu: '#menu',
+      sectionsColor: ['#000', '#f6ab00', '#7BAABE', 'whitesmoke', '#ccddff'],
+      anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+      menu: '#menu',
 
-    	afterLoad: function(anchorLink, index){
-        	var loadedSection = $(this);
+      afterLoad: function(anchorLink, index){
+          var loadedSection = $(this);
 
-        	//using index
-        	if(index == 1){
-        		var video = document.querySelector('video');
-          		video.play();
-          		var height = $('.home-mv-video').height();
-          		$('.home-mv-dot').height(height);
-        	}
-      	}
+          //using index
+          if(index == 1){
+            var video = document.querySelector('video');
+              video.play();
+              var height = $('.home-mv-video').height();
+              $('.home-mv-dot').height(height);
+          }
+        }
     });
 
-	var video = document.querySelector('video');
-	video.addEventListener('loadeddata', function(){
-		video.play();
-	});
+  var video = document.querySelector('video');
+  video.addEventListener('loadeddata', function(){
+    video.play();
+  });
 });
